8a4938b697a7a6cad8452f4fc8686227be83daad
veales <107602319+vea1es1337@users.noreply.github.com>
2022-12-23 14:49:26 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+pygame.init()
+pygame.display.set_mode((400, 500))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
